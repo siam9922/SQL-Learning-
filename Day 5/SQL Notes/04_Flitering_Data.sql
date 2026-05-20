4c86655cc20ffacd0463cbee360b72e7d3cc8aae
Siam Hasan <siamhasan773@gmail.com>
2026-05-19 22:29:13 -0400

    Learned LIKE and got introduced to JOINS

---
+++ Day 5/SQL Notes/04_Flitering_Data.sql
@@ -0,0 +1,28 @@
+-- WHERE OPERATOR 
+
+-- 	5. SEARCH OPERATOR LIKE and ILIKE 
+	
+	-- Find all customers whose first_name starts with 'M'
+
+	--SELECT * 
+	--FROM customers
+	--WHERE first_name LIKE 'M%'
+
+	-- Find all customers whose first_name starts with 'n'
+
+	--SELECT * 
+	--FROM customers
+	--WHERE first_name LIKE 'n%'
+
+	-- Find all customers whose first_name contains 'r'
+
+	--SELECT *
+	--FROM customers
+	--WHERE first_name LIKE '%r%'
+
+	/* Find all customers whose first_name 
+	has 'r' in the 3rd position */
+
+	--SELECT * 
+	--FROM customers
+	--WHERE first_name LIKE '_r%'
---
+++ Day 5/SQL Notes/05_Sql_ Joins.sql
@@ -0,0 +1,39 @@
+
+-- SQL JOIN BASICS
+
+-- JOIN are wider tables that are combined with coloumns horizontally 
+-- Set operators combine rows and keep them vertically 
+
+
+-- JOIN TYPES:			SET TYPES:
+
+-- 1. INNER JOIN		- UNION
+-- 2. FULL JOIN			- UNION ALL
+-- 3. LEFT JOIN			- EXCEPT (MINUS)
+-- 4. RIGHT JOIN		- INTERSECT
+-- 5. Left Anti Join
+-- 6. Right Anti Join
+-- 7. Full Anti Join
+-- 8. Cross Join
+
+
+-- Why we use JOINS?
+
+-- RECOMBINE DATA
+-- DATA ENRICHMENT "Getting Extra Data"
+-- Check Existence "Filtering"
+
+
+-- Exercises 
+
+
+-- 1. No Join
+
+/* Retrieve all data from customers and order in two different results */
+
+--SELECT * FROM customers;
+--SELECT * FROM orders;
+
+
+-- 2. Inner JOIN
+ 
